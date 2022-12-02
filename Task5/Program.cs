6331b28c6df29ec79f421fda51bdea055ad60c53
Nickolas <kovcheg.studio.2014@gmail.com>
2022-12-02 11:09:08 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-ое число: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b || b * b == a)
+    Console.WriteLine("yes");
+else 
+    Console.WriteLine ("no");
+
+
+
